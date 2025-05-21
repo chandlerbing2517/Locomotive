6791ca9d5e3c993206a864901d42fe5a10ac32d0
JAIN Sajal <sajal.jain@soprasteria.com>
2025-05-21 19:30:50 +0530

    A navbar with every element appears 1 sec delay

--- script.js
+++ script.js
@@ -1,23 +1,7 @@
-var tl=gsap.timeline()
-tl.to("#box1",{
-    x:1000,
-    rotate:360,
-    duration:2,
-    backgroundColor: "black",
-    delay:2,
-    scale:0.7,
-})
-tl.to("#box2",{
-    x:1000,
-    rotate:360,
-    duration:2,
-    backgroundColor: "blue",
-    scale:0.7,
-})
-tl.to("#box3",{
-    x:1000,
-    rotate:360,
-    duration:2,
-    backgroundColor: "red",
-    scale:0.7,
+gsap.from("#navbar h3",{
+    delay:0.5,
+    y:-50,
+    opacity:0,
+    duration:1,
+    stagger:1   
 })
