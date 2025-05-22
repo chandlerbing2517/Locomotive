4fdf518a377af39e6a0efa28f279ee256e00a9a9
JAIN Sajal <sajal.jain@soprasteria.com>
2025-05-22 20:16:34 +0530

    Completed MiniP

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-gsap.from("#navbar h3",{
+var tl=gsap.timeline();
+tl.from("#navbar h3",{
     delay:0.5,
     y:-50,
     opacity:0,
     duration:1,
-    stagger:1   
+    stagger:0.3
+})
+tl.from("#main h1",{
+    x:-500,
+    opacity:0,
+    stagger:0.5,
+    duration:2,
+})
+tl.from("img",{
+    x:100,
+    opacity:0,
+    rotate:45,
+    stagger:0.5,
+    duration:2,
 })
